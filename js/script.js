74982e1545a8ebba332676fc6e08b3099dac05a5
calebarapp <36084590+calebarapp@users.noreply.github.com>
2019-01-18 11:43:41 -0800

    Update projects page

--- js/script.js
+++ js/script.js
@@ -45,10 +45,57 @@ window.onload = function menu() {
     }
 
     disableSelect(document.getElementById('buttonTop'));
-    disableSelect(document.getElementById('buttonMid'));
+    disableSelect(document.getElementById('buttonMid'))
     disableSelect(document.getElementById('buttonBottom'));
 
     disableSelect(document.getElementById('dropdownMenu'));
     disableSelect(document.getElementById('dropdownButton'));
 }
 //End navigation-------------------------------------------------------------------------------
+
+//projects dropdown----------------------------------------------------------------------------
+//document.getElementById("dropDownBox").addEventListener("mouseover", function () {
+//    console.log('test');
+//    document.getElementById("dropDownButton").classList.add("projects__dropdown--active");
+//    let dropdownList = document.getElementsByClassName("projects__dropdown-item");
+//    for (let x = 0; x < dropdownList.length; x++) {
+//        dropdownList[x].classList.add("projects__dropdown-item--active");
+//    }
+//});
+
+//document.getElementById("dropDownBox").addEventListener("mouseout", function () {
+//    document.getElementById("dropDownButton").classList.remove("projects__dropdown--active");
+    
+//})
+
+//Modal----------------------------------------------------------------------------------------
+
+document.getElementById("rightArrowBox").addEventListener("mouseover", function () {
+    console.log("test");
+    document.getElementById("rightArrow").style.fill= "white";
+})
+
+document.getElementById("rightArrowBox").addEventListener("mouseout", function () {
+    console.log(document.getElementById("rightArrow").style.fill);
+    document.getElementById("rightArrow").style.fill = "black";
+})
+
+document.getElementById("leftArrowBox").addEventListener("mouseover", function () {
+    console.log("test");
+    document.getElementById("leftArrow").style.fill = "white";
+})
+
+
+document.getElementById("leftArrowBox").addEventListener("mouseout", function () {
+    console.log(document.getElementById("rightArrow").style.fill);
+    document.getElementById("leftArrow").style.fill = "black";
+
+})
+
+document.getElementById("modalExit").addEventListener('click', function () {
+    document.getElementById("modalContainer").style.display = "none";
+});
+
+document.getElementById("modal").addEventListener('click', function () {
+    document.getElementById("modalContainer").style.display = "block";
+});
