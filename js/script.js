80b46c9d7fa8ed5e68b09f80cee527af8ee72c9d
calebarapp <calebarapp@gmail.com>
2019-03-27 12:36:06 -0700

    fix experience buttons

--- js/script.js
+++ js/script.js
@@ -52,3 +52,29 @@ window.onload = function menu() {
     disableSelect(document.getElementById('dropdownButton'));
 }
 //End navigation-------------------------------------------------------------------------------
+//Experience page dropdown button 
+// on dropdown click
+document.getElementById('projectsDropDownButton').addEventListener('click', projectsDropDown);
+document.getElementById('techAcadDropDownButton').addEventListener('click', techAcadDropDown);
+// on mouseout
+document.getElementById('projectsDropDown').addEventListener('mouseleave', clearDropdowns);
+document.getElementById('techAcadDropDown').addEventListener('mouseleave', clearDropdowns);
+
+//ons
+function projectsDropDown() { 
+    document.getElementById('projectsDropDownContainer').classList.toggle('button-dropdown__container--active');
+    document.getElementById('projectsDropDownButton').classList.toggle('button-dropdown__item-top--activated');
+}
+
+function techAcadDropDown() { 
+    document.getElementById('techAcadDropDownContainer').classList.toggle('button-dropdown__container--active');
+    document.getElementById('techAcadDropDownButton').classList.toggle('button-dropdown__item-top--activated');
+}
+
+function clearDropdowns() { 
+    document.getElementById('techAcadDropDownContainer').classList.remove('button-dropdown__container--active');
+    document.getElementById('techAcadDropDownButton').classList.remove('button-dropdown__item-top--activated');
+    document.getElementById('projectsDropDownContainer').classList.remove('button-dropdown__container--active');
+    document.getElementById('projectsDropDownButton').classList.remove('button-dropdown__item-top--activated');
+}
+
